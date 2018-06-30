80e5e6cebaf5a923f12d74fc6687245bdde3f304
kungfumas <eri299@gmail.com>
2018-06-30 08:17:17 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)HelloWorld.java
+ *
+ * HelloWorld application
+ *
+ * @author 
+ * @version 1.00 2018/3/10
+ */
+ 
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.print("Hello World!");
+    	System.out.println("Belajar Pemrograman!");
+    }
+}
